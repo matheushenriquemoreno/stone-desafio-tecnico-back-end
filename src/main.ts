33c27927545733e6de63f094a05bd48144f08c7c
Matheus Henrique <69221000+matheushenriquemoreno@users.noreply.github.com>
2026-09-04 14:19:50 -0300

    feat: Adicionando suporte a documentação do scalar.

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { createCorsOptions } from './shared/presentation/http/cors-options';
 import { configureTrustedProxies } from './shared/presentation/http/trusted-proxies';
 import { setupOpenApi } from './shared/presentation/openapi/setup-openapi';
 import { PublicValidationPipe } from './shared/presentation/validation/public-validation.pipe';
+import { setupScalarDocs } from './shared/presentation/openapi/scalar-docs-config';
 
 export async function bootstrap(): Promise<void> {
   createAppConfig(process.env);
@@ -15,12 +16,16 @@ export async function bootstrap(): Promise<void> {
 
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<AppConfig>);
+  const port = configService.getOrThrow<number>('port');
+
+
   configureTrustedProxies(app, configService.getOrThrow('trustedProxyIps'));
   app.enableCors(createCorsOptions(configService.getOrThrow('allowedOrigins')));
   app.useGlobalPipes(new PublicValidationPipe());
-  const port = configService.getOrThrow<number>('port');
-  setupOpenApi(app, configService.getOrThrow('cookieName'));
 
+  setupOpenApi(app, configService.getOrThrow('cookieName'));
+  setupScalarDocs(app);
+  
   await app.listen(port);
 }
 
